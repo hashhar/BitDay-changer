3b2028095590d36c06dce43af936e29cb57d3cc1
Ashhar Hasan <hashhar_dev@outlook.com>
2016-03-08 14:21:39 +0530

    Fix change.sh

--- change.sh
+++ change.sh
@@ -2,8 +2,10 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-sessionfile=`find "${HOME}/.dbus/session-bus/" -type f`
-export `grep "DBUS_SESSION_BUS_ADDRESS" "${sessionfile}" | sed '/^#/d'`
+PID=$(pgrep gnome-session)
+for i in $PID; do
+	export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$i/environ|cut -d= -f2-);
+done
 
 files=(01-Early-Morning.png 02-Mid-Morning.png 03-Late-Morning.png 04-Early-Afternoon.png 05-Mid-Afternoon.png 06-Late-Afternoon.png 07-Early-Evening.png 08-Mid-Evening.png 09-Late-Evening.png 10-Early-Night.png 11-Mid-Night.png 12-Late-Night.png
 );
@@ -16,17 +18,13 @@ hour=$(echo $hour | sed 's/^0*//')
 
 case $XDG_CURRENT_DESKTOP in
 	Mint|Mate) setcmd="gsettings set org.mate.background picture-uri";;
-	Cinnamon) setcmd="gsettings set org.cinnamon.background picture-uri";;	
+	Cinnamon) setcmd="gsettings set org.cinnamon.background picture-uri";;
+	i3) setcmd="feh --bg-fill";;
 	*) setcmd="gsettings set org.gnome.desktop.background picture-uri";; # GNOME/Unity, default
 esac
-if [[ -z $XDG_CURRENT_DESKTOP ]]; then # Fallback for i3
-	case $DESKTOP_SESSION in
-		i3) setcmd="feh --bg-max"
-	esac
-fi
 
-for i in {13..0..-1}; do # Loop backwards through the wallpapers 
-    if [[ $hour -le ${timing[i]} ]]; then
+for i in {11..0..-1}; do # Loop backwards through the wallpapers 
+    if [[ $hour -ge ${timing[i]} ]]; then
 	$setcmd file://$DIR/${files[i]}
         echo "Wallpaper set to ${files[i]}"
         exit
